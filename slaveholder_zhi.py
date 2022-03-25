0c8d8b1cb018e31254b6f466702fc3f06972f0c6
zhiq <ziyin810@gmail.com>
2022-03-25 16:27:26 +0800

    little changed

--- slaveholder_zhi.py
+++ slaveholder_zhi.py
@@ -23,21 +23,10 @@ is_config = os.path.exists(os.path.join(current_dir_path, 'holder_zhi.json'))
 a_b = 0
 if is_config:
     chose_config = input('检测到配置文件存在，是否跳过手动输入？(回车默认跳过，输入任意字符开始手动输入): ')
-    a_b = chose_config
+    a_b = 1 if not chose_config else 0
     print()
 
 if not a_b:
-    holder = 'holder_zhi.json'
-    with open(holder, 'r', encoding='utf8') as cs: configs = json.load(cs)
-
-    labels = os.path.join(configs['Data'], 'Labels')
-    images = os.path.join(configs['Data'], 'Images')
-    sub_sce = configs['Scene']
-    main_sce = configs['Scene_Main']
-    service_eng = configs['Host&Port']
-    file_name = configs['Excel_Name']
-    limit_client_session = configs['Limit_Num']
-else:
     default_data = os.path.join(current_dir_path, 'data')
     default_labs, default_imgs = os.path.join(default_data, 'Labels'), os.path.join(default_data, 'Images')
     inp_data = input('1. 请输入数据文件夹名(回车即默认“data”): ')
@@ -54,10 +43,25 @@ else:
     service_eng = input('5. (必选项)请输入OCR服务地址(务必保证当前机器与之相互连通): ')
     while not service_eng: service_eng = input('必须输入OCR服务地址(如“http://188.88.88.88:8506/”): ')
     print()
+    if service_eng[-1] != '/': service_eng += '/'
+    p = Peons(labels, images, sub_sce, main_sce, service_eng, file_name, limit_client_session)
+    p.work_work()
+else:
+    try:
+        with open('holder_zhi.json', 'r', encoding='utf8') as cs: configs = json.load(cs)
+    except: print('请检查"holder_zhi.json"的内容')
+    else:
+        labels = os.path.join(configs.get('Data'), 'Labels')
+        images = os.path.join(configs.get('Data'), 'Images')
+        sub_sce = configs['Scene']
+        main_sce = configs['Scene_Main']
+        service_eng = configs['Host&Port']
+        file_name = configs['Excel_Name']
+        limit_client_session = configs['Limit_Num']
 
-if service_eng[-1] != '/': service_eng += '/'
-p = Peons(labels, images, sub_sce, main_sce, service_eng, file_name, limit_client_session)
-p.work_work()
+        if service_eng[-1] != '/': service_eng += '/'
+        p = Peons(labels, images, sub_sce, main_sce, service_eng, file_name, limit_client_session)
+        p.work_work()
 
 print('5秒后关闭本程序......')
 time.sleep(5)
